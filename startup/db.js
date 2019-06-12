888fda5f05405f02e07509cd42bfb10f2887b7f1
Joswal <princejoswal@gmail.com>
2019-06-12 20:16:31 +0100

    changed the prod db details

--- startup/db.js
+++ startup/db.js
@@ -4,5 +4,6 @@ const config = require('config');
 module.exports = function () {
   const db = config.get('db');
   mongoose.connect(db)
-    .then(() => console.log(`Connected to ${db}...`));
+    .then(() => console.log(`Connected to ${db}...`))
+    .catch(err => console.error(err))
 }
